3012d0e1b30df69075a775a79d5c4138206cb68c
Gabriel Vian <g@gabrielvian.com>
2019-07-09 13:58:37 -0400

    add final part of challenge: numbers over 10

--- starters/texttonum.rb
+++ starters/texttonum.rb
@@ -1,30 +1,94 @@
 NUMS = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-    'ten': 10
+  'zero': 0,
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+  'ten': 10
 }.freeze
 
-# TODO: add 10 powers support (thirty-one)
+TENS = {
+  'eleven': 11,
+  'twelve': 12,
+  'thirteen': 13,
+  'fourteen': 14,
+  'fifteen': 15,
+  'sixteen': 16,
+  'seventeen': 17,
+  'eighteen': 18,
+  'nineteen': 19,
+  'twenty': 20
+}.freeze
+
+TENPOWS = {
+  'twenty': 20,
+  'thirty': 30,
+  'forty': 40,
+  'fifty': 50,
+  'sixty': 60,
+  'seventy': 70,
+  'eighty': 80,
+  'ninety': 90
+}.freeze
+
 def text_to_num(text)
+  puts "default: #{text}"
+  text = subcomp(text)
+  text = subsmall(text)
+
+  text = subops(text)
+
+  text = text.split(' ')
+  text = divmul(text)
+  text = addsub(text)
+
+  text[0].split('.')[1] == '0' ? text[0].to_i : text[0].to_f
+end
+
+# Substitutes numbers in range 0-10
+def subsmall(text)
+  TENPOWS.keys.each do |k|
+    text.gsub!(k.to_s, TENPOWS[k].to_s)
+  end
+  TENS.keys.each do |k|
+    text.gsub!(k.to_s, TENS[k].to_s)
+  end
+  NUMS.keys.each do |k|
+    text.gsub!(k.to_s, NUMS[k].to_s)
+  end
+  text
+end
+
+# Substitutes numbers over 0-10 range with units from 1-9
+def subcomp(text)
+  text = text.split(' ')
+  i = 0
+  while i < text.length
+    if %r{\w+\-\w+}.match?(text[i])
+      res = text[i].split('-')
+      text[i] = (TENPOWS[res[0].to_sym] + NUMS[res[1].to_sym]).to_s
+      i = -1
+    end
+    i += 1
+  end
+  text.join(' ')
+end
+
+# Substitutes math operators
+def subops(text)
   text.gsub!('divided by', '/')
   text.gsub!('times', '*')
   text.gsub!('minus', '-')
   text.gsub!('plus', '+')
+  text
+end
 
-  NUMS.keys.each do |k|
-    text.gsub!(k.to_s, NUMS[k].to_s)
-  end
-
-  # Divisions and multiplications
-  text = text.split(' ')
+def divmul(text)
   i = 0
   while i < text.length
     if %r{\d*.?\d+ \/ \d+.?\d*}.match?(text[i..i + 2].join(' '))
@@ -44,8 +108,10 @@ def text_to_num(text)
     end
     i += 1
   end
+  text
+end
 
-  # Addition and subtraction
+def addsub(text)
   i = 0
   while i < text.length
     if %r{\d*.?\d+ \+ \d+.?\d*}.match?(text[i..i + 2].join(' '))
@@ -65,5 +131,5 @@ def text_to_num(text)
     end
     i += 1
   end
-  text[0].split('.')[1] == '0' ? text[0].to_i : text[0].to_f
+  text
 end
